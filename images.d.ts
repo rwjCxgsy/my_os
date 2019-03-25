37b7cd0f5cd948506b35cfa22f66c5f1e757c522
DESKTOP-A38OOT2\rwj84 <renweijun@huianrong.com>
2019-03-25 18:01:38 +0800

    新增申明文件

--- images.d.ts
+++ images.d.ts
@@ -1,1 +1,33 @@
+declare module '*.svg'
+declare module '*.png'
+declare module '*.jpg'
+declare module '*.jpeg'
+declare module '*.gif'
+declare module '*.bmp'
+declare module '*.tiff'
+declare module 'rc-form'
 declare module '*.less'
+declare module 'query-string'
+declare module 'china-division/*.json'
+declare module 'react-svg'
+declare module 'jsonp'
+// declare const AMap: any
+// declare const xhr: any
+
+
+// declare interface IMapData {
+//     position: number[]
+//     icon: string
+//     offset?: any
+//     title: string
+//     img?: string
+//     description?: string,
+//     date: string,
+//     address: string,
+//     addressCode: number
+// }
+
+
+// declare module 'react-onsenui'
+// declare module 'react-router-dom'
+// declare module 'react-router'
